e62caae452f119fdd92de2a10f10e0a84694f353
moshekarmel1 <moshekarmel1@gmail.com>
2015-11-30 12:16:24 -0500

    added authentication

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,6 +1,6 @@
 var app = angular.module('parnassah', ['ui.router']);
 
-app.factory('posts', ['$http', function($http){
+app.factory('posts', ['$http', 'auth', function($http, auth){
     var o = {
         posts: []
     };
@@ -12,13 +12,17 @@ app.factory('posts', ['$http', function($http){
     };
 
     o.create = function(post) {
-        return $http.post('/posts', post).success(function(data){
+        return $http.post('/posts', post, {
+            headers: {Authorization: 'Bearer ' + auth.getToken()}
+        }).success(function(data){
             o.posts.push(data);
         });
     };
 
     o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote')
+        return $http.put('/posts/' + post._id + '/upvote', null, {
+            headers: {Authorization: 'Bearer ' + auth.getToken()}
+        })
         .success(function(data){
             post.upvotes += 1;
         });
@@ -31,11 +35,15 @@ app.factory('posts', ['$http', function($http){
     };
 
     o.addComment = function(id, comment) {
-        return $http.post('/posts/' + id + '/comments', comment);
+        return $http.post('/posts/' + id + '/comments', comment, {
+            headers: {Authorization: 'Bearer ' + auth.getToken()}
+        });
     };
 
     o.upvoteComment = function(post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+        return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+            headers: {Authorization: 'Bearer ' + auth.getToken()}
+        })
         .success(function(data){
           comment.upvotes += 1;
     });
@@ -65,12 +73,80 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
                 return posts.get($stateParams.id);
             }]
         }
+    })
+    .state('login', {
+        url: '/login',
+        templateUrl: '/login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function($state, auth){
+            if(auth.isLoggedIn()){
+                $state.go('home');
+            }
+        }]
+    })
+    .state('register', {
+    url: '/register',
+    templateUrl: '/register.html',
+    controller: 'AuthCtrl',
+    onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+            $state.go('home');
+        }
+        }]
     });
     $urlRouterProvider.otherwise('home');
+}])
+
+.factory('auth', ['$http', '$window', function($http, $window){
+    var auth = {};
+    //save jwt token in local storage
+    auth.saveToken = function (token){
+        $window.localStorage['parnassah-token'] = token;
+    };
+    //get jwt token from local storage
+    auth.getToken = function (){
+        return $window.localStorage['parnassah-token'];
+    }
+    //is the user logged in?
+    auth.isLoggedIn = function(){
+        var token = auth.getToken();
+        if(token){
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+    //get current user
+    auth.currentUser = function(){
+        if(auth.isLoggedIn()){
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.username;
+        }
+    };
+    //register route
+    auth.register = function(user){
+        return $http.post('/register', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+    //login route
+    auth.logIn = function(user){
+        return $http.post('/login', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logOut = function(){
+        $window.localStorage.removeItem('parnassah-token');
+    };
+
+    return auth;
 }]);
 
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
-    $scope.test = 'Hello world!';
+app.controller('MainCtrl', ['$scope', 'posts', 'auth', function($scope, posts, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
     $scope.posts = posts.posts;
 
     $scope.addPost = function(){
@@ -90,7 +166,8 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     };
 }]);
 
-app.controller('PostsCtrl', ['$scope', 'posts', 'post', function($scope, posts, post){
+app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth', function($scope, posts, post, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
     $scope.post = post;
 
     $scope.addComment = function(){
@@ -107,4 +184,30 @@ app.controller('PostsCtrl', ['$scope', 'posts', 'post', function($scope, posts, 
     $scope.incrementUpvotes = function(comment){
         posts.upvoteComment(post, comment);
     };
+}]);
+
+app.controller('AuthCtrl', ['$scope', '$state', 'auth', function($scope, $state, auth){
+    $scope.user = {};
+
+    $scope.register = function(){
+        auth.register($scope.user).error(function(error){
+            $scope.error = error;
+        }).then(function(){
+            $state.go('home');
+        });
+    };
+
+    $scope.logIn = function(){
+        auth.logIn($scope.user).error(function(error){
+            $scope.error = error;
+        }).then(function(){
+            $state.go('home');
+        });
+    };
+}]);
+
+app.controller('NavCtrl', ['$scope', 'auth', function($scope, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
 }]);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,45 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var passport = require('passport');
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
+//authentication middleware
+var auth = jwt({
+    secret: 'SECRET', userProperty: 'payload'
+});
+//route to register new user
+router.post('/register', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+    var user = new User();
+    user.username = req.body.username;
+    user.setPassword(req.body.password)
+    user.save(function (err){
+    if(err){ return next(err); }
+        return res.json({
+            token: user.generateJWT()
+        });
+    });
+});
+//login route
+router.post('/login', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+    passport.authenticate('local', function(err, user, info){
+        if(err){ return next(err); }
+        if(user){
+            return res.json({
+                token: user.generateJWT()
+            });
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+});
+
 //route to get all posts
 router.get('/posts', function(req, res, next) {
     Post.find(function(err, posts){
@@ -13,8 +52,9 @@ router.get('/posts', function(req, res, next) {
     });
 });
 //route to post a post!
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
     post.save(function(err, post){
         if(err){
             return next(err);
@@ -46,7 +86,7 @@ router.get('/posts/:post', function(req, res, next) {
     });
 });
 //post upvote w/ put
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
     req.post.upvote(function(err, post){
         if (err) {
             return next(err);
@@ -58,9 +98,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 COMMENTS SECTION
 */
 //post comment
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
     comment.save(function(err, comment){
         if(err){
             return next(err);
@@ -89,7 +130,7 @@ router.param('comment', function(req, res, next, id) {
     });
 });
 //post comment upvote
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
     req.comment.upvote(function(err, comment){
         if (err) {
             return next(err);
